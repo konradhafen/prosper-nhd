527b63dd2dc3bcf68fa7f113f420e97e30f26aa2
KONRADSPC\khafe <konrad.hafen@gmail.com>
2018-01-16 15:51:19 -0800

    calculating zonal stats

--- precipitation_for_quadextent.R
+++ precipitation_for_quadextent.R
@@ -9,3 +9,12 @@ datadir = "E:/konrad/Projects/usgs/prosper-nhd/data"
 setwd(datadir)
 library(raster)
 library(rgdal)
+
+
+# Load precip raster ------------------------------------------------------
+
+pptbrick <- brick("ppt/wateryear/ppt.tif")
+NAvalue(pptbrick) <- 0.0
+quads <- readOGR("quads", "Historical_Topo_Maps_subset")
+
+quadmean <- extract(pptbrick, quads, fun = mean, df = T, na.rm = T)
