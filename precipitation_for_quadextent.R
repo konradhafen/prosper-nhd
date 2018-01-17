3fe9b23275d0a9f203009feab49747ca662f302c
KONRADSPC\khafe <konrad.hafen@gmail.com>
2018-01-17 12:17:23 -0800

    zonal stats

--- precipitation_for_quadextent.R
+++ precipitation_for_quadextent.R
@@ -14,7 +14,18 @@ library(rgdal)
 # Load precip raster ------------------------------------------------------
 
 pptbrick <- brick("ppt/wateryear/ppt.tif")
-NAvalue(pptbrick) <- 0.0
+NAvalue(pptbrick) <- 0.0 #set 0.0 to NA
+
+# Calculate mean and sd and save new rasters ------------------------------
+
+pptmean <- calc(pptbrick, fun=mean, na.rm=T)
+pptsd <- calc(pptbrick, fun=sd, na.rm=T)
+writeRaster(pptmean, "ppt/wateryear/pptmean.tif")
+writeRaster(pptsd, "ppt/wateryear/pptsd.tif")
+
+# Load quad polygons ------------------------------------------------------
+
 quads <- readOGR("quads", "Historical_Topo_Maps_subset")
 
 quadmean <- extract(pptbrick, quads, fun = mean, df = T, na.rm = T)
+quadsd <- extract(pptbrick, quads, fun = sd, df = T, na.rm = T)
