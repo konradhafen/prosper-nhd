21065f1a7a37d2c7f1be0934cdaca84e41934d93
KONRADSPC\khafe <konrad.hafen@gmail.com>
2018-03-27 13:32:37 -0700

    more tweaking

--- prosper_class_analyze.R
+++ prosper_class_analyze.R
@@ -14,18 +14,35 @@ indat <- read.csv(paste(wd,fn,sep="/"))
 # Subset to perennial/intermittnet only and classify overall --------------
 
 pidat <- subset(indat, indat$FCODE==46006 | indat$FCODE==46003)
-pidat$nclass <- ifelse(pidat, pidat$FCODE==46006, "Perennial", "Intermittent")
+pidat$nclass <- ifelse(pidat$FCODE==46006, "Perennial", "Intermittent")
 
 #classify reaches as perennial or intermittent based on pwet
 
 #cutoff proportion for perennial
 cutoff <- 0.75
 pidat$pclass <- ifelse(pidat$pwet >= cutoff, "Wet", "Dry")
+#run functions section first
+pidat$dclass <- mapply(misclass_type, pidat$nclass, pidat$pclass)
 
 #melt
 pidat.melt <- melt(pidat)
 
 
-# Analysis ----------------------------------------------------------------
+# functions ---------------------------------------------------------------
 
+misclass_type <- function(nhdclass, prosperclass)
+{
+  if ((nhdclass == "Perennial" & prosperclass=="Wet") | (nhdclass == "Intermittent" & prosperclass=="Dry"))
+  {
+    return ("same")
+  }
+  else if ((nhdclass == "Perennial" & prosperclass=="Dry"))
+  {
+    return ("nhd wet prosper dry")
+  }
+  else if ((nhdclass == "Intermittent" & prosperclass=="Wet"))
+  {
+    return("nhd dry prosper wet")
+  }
+}
 
