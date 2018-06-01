47b2d2d73b54bbf37255afd1d3e19541152fbc94
KONRADSPC\khafe <konrad.hafen@gmail.com>
2018-06-01 10:51:05 -0700

    added code for logisitc regression

--- misclassification_regression.R
+++ misclassification_regression.R
@@ -1,19 +1,49 @@
+
 # Do setup ----------------------------------------------------------------
 
 rm(list=ls())
-library(ggplot2)
-library(reshape2)
+library(tidyverse)
 
-wd <- "E:/konrad/Projects/usgs/prosper-nhd/data/outputs/csv"
-fn <- "nhd_prosper_cat_quads_mr.csv"
+wd <- "E:\\konrad\\Projects\\usgs\\prosper-nhd\\data\\outputs\\csv"
+fn <- "nhd_crb_hr_split_perintap_buf20.csv"
 
-indat <- read.csv(paste(wd,fn,sep="/"))
+indat <- read_csv(fn)
 
 
-# Misclassification regression --------------------------------------------
+# Define functions --------------------------------------------------------
 
-lm1 <- lm(disagree ~ pdsi_mean, data=indat)
-summary(lm1)
+misclass_type_cat <- function(fcode, catval)
+{
+  if (((fcode == 46006 | fcode==55800) & catval > 0) | ((fcode != 46006 | fcode != 55800) & catval < 0))
+  {
+    return ("Agree")
+  }
+  else if ((fcode == 46006 | fcode==55800) & catval < 0)
+  {
+    return ("NHD wet PROSPER dry")
+  }
+  else if ((fcode != 46006 | fcode != 55800) & catval > 0)
+  {
+    return("NHD dry PROSPER wet")
+  }
+  else
+  {
+    return ("Invalid")
+  }
+}
 
-plot(indat$pdsi_mean, indat$disagree)
-abline(0.329, 0.0018)
+# Subset and calculate misclassifications ---------------------------------
+
+keeps <- c("id", "FCODE", "X16_maj", "X16_mean")
+wrkdat <- indat[, keeps]
+
+wrkdat$mc_type <- misclass_type_cat(wrkdat$FCODE, wrkdat$X16_maj)
+wrkdat <- subset(wrkdat, wrkdat$misclass != "Invalid")
+wrkdat$mc <- ifelse(wrkdat$mc_type == "Agree", 0, 1)
+
+
+# Logistic regression -----------------------------------------------------
+
+logmodel <- glm(mc ~ X16_maj, data=wrkdat, family=binomial(link="logit"))
+
+
