470d245329495770a247ac1a5e67645481ac8bc4
KONRADSPC\khafe <konrad.hafen@gmail.com>
2018-09-12 09:06:49 -0700

    logistic regression with EROM values

--- obs_nhd_mr_erom.R
+++ obs_nhd_mr_erom.R
@@ -21,8 +21,8 @@ indat.month <- indat.month[!(indat.month$Category=="Wet" & indat.month$Month<8),
 keeps.month <- c("Category", "FCODE", "Q0001F_1", "Q0001F_12", "Q0001F__13", "Q0001F__14", "Q0001F__15", "Q0001F__16",
                  "Q0001F__17", "Q0001F__18", "Q0001F__19", "Q0001F__20", "Q0001F__21", "Q0001F__22")
 indat.month <- indat.month[,keeps.month]
-colnames(indat.month) <- c("Category", "FCODE", "1", "2", "3", "4", "5", "6",
-                           "7", "8", "9", "10", "11", "12")
+colnames(indat.month) <- c("Category", "FCODE", "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+                           "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
 
 indat.dry <- indat[!(indat$Category=="Wet" & indat$Month<8),]
 indat.lowq <- indat[!(indat$Category=="Wet" & indat$Month<8) & indat$Q0001F < 250,]
@@ -31,6 +31,47 @@ indat.lowq <- indat[!(indat$Category=="Wet" & indat$Month<8) & indat$Q0001F < 25
 # Classify reaches as permaenet or nonpermanent ---------------------------
 
 indat$nhdclass <- mapply(nhdclass, indat$FCODE)
+indat.month$nhdclass <- mapply(nhdclass, indat.month$FCODE)
+indat.month$mctype <- mapply(misclass_type, indat.month$nhdclass, indat.month$Category)
+indat.month$mc <- ifelse(indat.month$mctype == "Agree", 0, 1)
+
+
+# Test misclass as a function of baseflow (sept flow) ---------------------
+
+library(broom)
+library(pscl)
+
+indat.month$bfzero <- ifelse(indat.month$Sep == 0, 1, 0)
+
+lr.cat <- glm(mc ~ Category, data=indat.month, family="binomial")
+lr.bf <- glm(mc ~ Sep, data=indat.month, family="binomial")
+lr.catbf <- glm(mc ~ Category + Sep, data=indat.month, family="binomial")
+lr.catbfint <- glm(mc ~ Category*Sep, data=indat.month, family="binomial")
+lr.0 <- glm(mc ~ bfzero, data=indat.month, family="binomial")
+lr.cat0  <- glm(mc ~ Category + bfzero, data=indat.month, family="binomial")
+lr.cat0int <- glm(mc ~ Category*bfzero, data=indat.month, family="binomial")
+
+pR2(lr.cat0int)
+pR2(lr.cat)
+pR2(lr.0)
+
+AICctab(lr.cat, lr.0, lr.cat0, lr.cat0int)
+
+plot.dat <- augment(lr.cat0int)
+ggplot(plot.dat, aes(bfzero, plogis(.fitted))) +
+  geom_line(aes(color=Category)) + 
+  geom_point(aes(bfzero, mc, colour=Category), alpha=0.1) +
+  labs(x = "EROM Sep", y = "Probability of disagreement") +
+  theme_bw()
+
+
+# Create ROC curve --------------------------------------------------------
+
+library(pROC)
+roc.data <- augment(lr.cat0int)
+roc.fit <- roc(mc ~ plogis(.fitted), data=roc.data)
+auc(roc.fit)
+plot.roc(roc.fit, xlim=c(0,1), ylim=c(0,1))
 
 
 # Boxplots of flow by NHD class -------------------------------------------
