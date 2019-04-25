c77f316e63dc9aa54a85c414e28649e0637f0ae0
KONRADSPC\khafe <konrad.hafen@gmail.com>
2019-04-25 09:22:42 -0700

    changing model figure

--- obs_nhd_misclass_analysis.R
+++ obs_nhd_misclass_analysis.R
@@ -342,25 +342,34 @@ model.data$pdsi_dif <- model.data$pdsidif1 + model.data$pdsidif2
 score <- qnorm((0.95/2) + 0.5)
 model.data$lwr <- plogis(model.data$.fitted-score*model.data$.se.fit)
 model.data$upr <- plogis(model.data$.fitted+score*model.data$.se.fit)
+model.data$panelname <- paste("Stream Order:", model.data$as.factor.StreamOrde.)
 
 ggplot(model.data, aes(pdsi_dif, plogis(.fitted))) +
   geom_ribbon(aes(x=pdsi_dif, ymin=lwr, ymax=upr, group=Category), alpha = 0.2) + 
   geom_line(aes(color=Category)) + 
   geom_point(aes(pdsi_dif, mc, colour=Category), alpha=0.1) +
+  scale_x_continuous(breaks=seq(-8,8,4)) +
   scale_color_manual(values=c("#fb0026", "#0c51fd")) +
-  facet_wrap(~as.factor.StreamOrde., ncol=3) + 
+  facet_wrap(~panelname, ncol=3) + 
   labs(x = "PDSI difference", y = "Probability of disagreement", color = "Observation type") +
-  ggtitle("Probability of Disagreement by Stream Order (NHDPlus-HR)") +
-  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+  #ggtitle("Probability of Disagreement by Stream Order (NHDPlus-HR)") +
+  theme(panel.grid.major = element_line(colour = 'gray60', size=0.01), panel.grid.minor = element_blank(),
         panel.background = element_rect(fill = "transparent"), axis.line = element_line(colour = "black"),
         axis.title=element_text(size=14), axis.text=element_text(size=12),
         plot.title=element_text(size=16, face="bold", margin=margin(t=0, r=0, b=20, l=0)),
         plot.background = element_rect(fill = "transparent", color = NA), 
         axis.title.y = element_text(margin=margin(t=0, r=20, b=0, l=0)),
-        axis.title.x = element_text(margin=margin(t=10, r=0, b=0, l=0))) +
-  theme(legend.position =c(0.97,0.22), legend.justification = c(1,1))
+        axis.title.x = element_text(margin=margin(t=10, r=0, b=0, l=0)),
+        panel.border = element_rect(colour = "black", fill=NA, size=0.5),
+        strip.text = element_text(size=12),
+        strip.background = element_rect(fill="transparent"),
+        legend.position =c(0.85,0.22), 
+        legend.justification = c(1,1), 
+        legend.title = element_text(size=14), 
+        legend.text = element_text(size=12))
 
-ggsave("C:/Users/khafe/Downloads/lr_model.png", plot = last_plot(), width = 10, height = 6.5, units = "in", bg = "transparent")
+ggsave("E:/konrad/Projects/usgs/prosper-nhd/figs/figs/lr_model.png", plot = last_plot(), 
+       width = 7, height = 4.5, units = "in", bg = "white", dpi=300)
 
 
 # Spline model ------------------------------------------------------------
